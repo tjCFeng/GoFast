8f26ee4cfaaa48228051d8dcda0d5cf2b4886d39
tjCFeng, LiuYang <tjCFeng@163.com>
2015-05-09 00:55:15 +0000

    Update Test.go

--- Test.go
+++ Test.go
@@ -3,8 +3,24 @@ package main
 import (
 	"time"
 	"fmt"
-	"github.com/tjCFeng/GoServer/DDNS"
+	"github.com/tjCFeng/GoFast/DDNS"
+	"github.com/tjCFeng/GoFast/Server"
 )
+
+func ClientOnAccept(Client *Server.ClientTCP) {
+	fmt.Println("Accept: ", Client.IPPort())
+}
+
+func ClientOnRead(Client *Server.ClientTCP) {
+	fmt.Println("Read: " + Client.GetDateTime().String())
+	Data, _ := Client.GetData()
+	Client.Send([]uint8(Data))
+	Client.Close()
+}
+
+func ClientOnClose(IPPort string) {
+	fmt.Println("Close: ", IPPort)
+}
 
 func main() {
 	ddns := DDNS.Inat123("username", "password", "domain", 3)
@@ -12,4 +28,16 @@ func main() {
 	time.Sleep(time.Minute * 10)
 	ddns.Stop()
 	fmt.Println(ddns.UpdateResult())
+	
+	serverTCP := new(Server.ServerTCP)
+	serverTCP.OnAccept = ClientOnAccept
+	serverTCP.OnRead = ClientOnRead
+	serverTCP.OnClose = ClientOnClose
+	serverTCP.Listen("80")
+	//server.AddBlackList("127.0.0.1")
+	defer serverTCP.Stop()
+
+	reader := bufio.NewReader(os.Stdin)
+	key, _, _:= reader.ReadLine()
+	if string(key) == "" {}
 }
